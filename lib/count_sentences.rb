c29238d324ceb640ee034544ea131c6dd587fdb4
Mahmoud Agamy <agamyv@gmail.com>
2018-11-21 21:49:23 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,9 +1,10 @@
 require 'pry'
 
+# Count_Sentences
 class String
 
   def sentence?
-    self.end_with?(",")
+    self.end_with?(".")
   end
 
   def question?
@@ -16,8 +17,12 @@ class String
 
   def count_sentences
     count = 0 
-    self.each do |word|
-      count += 1 if ",?!.".include?(word[-1])
+    i = 0
+    self.each_char.with_index do |char, idx|
+      if ("?!.".include?(char) && self[idx] != self[idx+1]) || ( (",".include?(char)) && (self[i+1..idx].split(" ").length > 4) )
+        count += 1
+        i = idx
+      end
     end
     count
   end
